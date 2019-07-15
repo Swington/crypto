27e740090339003a1248ab1cee90bbe1fa3b06bb
Swington <tomasz.switon@gmail.com>
2019-07-15 11:16:07 +0200

    Change crypto_main to use ShiftUnicode encryption and decryption

--- crypto.py
+++ crypto.py
@@ -1,11 +1,19 @@
-from encryptors import ShiftEncryptor
+from decryptors import ShiftUnicodeDecryptor
+from encryptors import ShiftEncryptor, ShiftUnicodeEncryptor
 
 
 def crypto_main():
+    target_operation = input()
+    if target_operation.lower() == "enc":
+        function = ShiftUnicodeEncryptor.encrypt
+    elif target_operation.lower() == "dec":
+        function = ShiftUnicodeDecryptor.decrypt
+    else:
+        return "Invalid target operation type"
     text = input()
     number = int(input())
-    encrypted_text = ShiftEncryptor.encrypt(text, number)
-    return encrypted_text
+    output_text = function(text, number)
+    return output_text
 
 
 if __name__ == '__main__':
--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -6,10 +6,29 @@ from crypto import crypto_main
 
 class TestCrypto(TestCase):
     @mock.patch.object(builtins, 'input')
-    def test_returns_valid_output(self, patched_input):
-        patched_input.side_effect = ['welcome to hyperskill', '5']
-        expected_output = "bjqhtrj yt mdujwxpnqq"
+    def test_returns_valid_output_on_encryption(self, patched_input):
+        patched_input.side_effect = ['enc', 'welcome to hyperskill', '5']
+        expected_output = "|jqhtrj%yt%m~ujwxpnqq"
 
         actual_output = crypto_main()
 
         self.assertEqual(expected_output, actual_output)
+
+    @mock.patch.object(builtins, 'input')
+    def test_returns_valid_output_on_decryption(self, patched_input):
+        patched_input.side_effect = ['dec', '|jqhtrj%yt%m~ujwxpnqq', '5']
+        expected_output = "welcome to hyperskill"
+
+        actual_output = crypto_main()
+
+        self.assertEqual(expected_output, actual_output)
+
+    @mock.patch.object(builtins, 'input')
+    def test_returns_message_on_invalid_target_operation_type(self, patched_input):
+        patched_input.side_effect = ['something', 'some string', 'some number']
+        expected_output = "Invalid target operation type"
+
+        actual_output = crypto_main()
+
+        self.assertEqual(expected_output, actual_output)
+
