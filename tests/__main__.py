90866e06142d8c6b0488de7b35f438635d23c391
Swington <tomasz.switon@gmail.com>
2019-07-15 13:51:31 +0200

    Refactor circleci config file to incorporate codacy-coverage

---
+++ tests/__main__.py
@@ -0,0 +1,8 @@
+import unittest
+
+if __name__ == '__main__':
+    loader = unittest.TestLoader()
+    start_dir = './'
+    suite = loader.discover(start_dir)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
